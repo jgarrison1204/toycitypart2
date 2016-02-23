db7ab304c1945d02000d95faee66e5789a8bbee0
Jacob Garrison <jgarrison1204@gmail.com>
2016-02-22 17:21:51 -0800

    Problem with printing to text file using calculate_products and calculate_brands methods

--- lib/app.rb
+++ lib/app.rb
@@ -13,23 +13,21 @@ def start
 end
 
 def create_report
-	#print_sales_report
-	print_products
-	loop_product_data
-	#print_brands
-	#loop_brand_data
-	File.open("report.txt", "w") do |somefile|
-		somefile.puts print_report_date
-		somefile.puts print_products
-		somefile.puts calculate_products
-		somefile.puts print_brands
-	end
+  loop_product_data
+  loop_brand_data
+  File.open("report.txt", "w") do |openfile|
+    openfile.puts print_report_date
+  	openfile.puts print_products
+    openfile.puts calculate_products
+  	openfile.puts print_brands
+    openfile.puts calculate_brands
+  end
 end
 
 # Print today's date
 def print_report_date
 	date = Time.now.strftime("%D")
-	"Sales report for #{date}"
+	 "Sales report for #{date}"
 end
 
 # Print "Products" in ascii art
@@ -46,7 +44,14 @@ def print_products
 	                                 "
 end
 
-#Products Report
+def dots
+	 "**************************"
+end
+
+def space
+
+end
+
 def loop_product_data
 	$products_hash["items"].each do |toy|
 		$toy_title = toy["title"]
@@ -61,37 +66,35 @@ def loop_product_data
 end
 
 def calculate_products
-	"	#{spacer}
-	#{print_toy_title($toy_title)}
-	#{print_toy_retail_price($retail_price)}
-	#{print_toy_total_sales($total_sales)}
-	#{calculate_average_sales($total_sales, $number_of_products)}
-	#{calculate_average_discount($total_sales, $retail_price, $number_of_products)}"
+"
+#{dots}
+#{print_toy_title}
+#{print_toy_retail_price}
+#{print_toy_total_sales}
+#{calculate_average_sales($total_sales, $number_of_products)}
+#{calculate_average_discount($total_sales,$retail_price, $number_of_products)}
+#{space}
+"
 end
 
-def spacer
-
-	"**************************"
+def print_toy_title
+	 $toy_title
 end
 
-def print_toy_title(toy_title)
-	$toy_title
+def print_toy_retail_price
+	 "Retail Price: $#{$retail_price}"
 end
 
-def print_toy_retail_price(retail_price)
-	 "Retail Price: $#{retail_price}"
-end
-
-def print_toy_total_sales(total_sales)
-	 "Total sales: $#{total_sales}"
+def print_toy_total_sales
+	 "Total sales: $#{$total_sales}"
 end
 
 def calculate_average_sales(total_sales, number_of_products)
-	 "Average sale: $#{total_sales/number_of_products}"
+	 "Average sale: $ #{total_sales/number_of_products}"
 end
 
 def calculate_average_discount(total_sales, retail_price, number_of_products)
-	 "Average discount: #{(1 - total_sales/(retail_price.to_f * number_of_products)).round(3)*100}%"
+	"Average discount: #{(1 - total_sales/(retail_price.to_f * number_of_products)).round(3)*100}"
 end
 
 # Print "Brands" in ascii art
@@ -102,8 +105,11 @@ def print_brands
 | |__  _ __ __ _ _ __   __| |___
 | '_ \\| '__/ _` | '_ \\ / _` / __|
 | |_) | | | (_| | | | | (_| \\__ \\
-|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+|_.__/|_|  \\__,_|_| |_|\\__,_|___/
+
+"
 end
+
 
 #Arrary containing unqiue brands used to create new arrarys by brand
 def dedup_brands
@@ -132,32 +138,31 @@ def loop_brand_data
 end
 
 def calculate_brands
-	spacer
-	print_brand_title
-	print_brand_stock
-	calculate_average__brand_price($retail_price, $brand_stock)
-	calculate_brand_revenue
+"
+#{dots}
+#{print_brand_title}
+#{print_brand_stock}
+#{calculate_average__brand_price($retail_price, $brand_stock)}
+#{calculate_brand_revenue}
+#{space}
+"
 end
 
 def print_brand_title
-	$brand_title
+	 $brand_title
 end
 
 def print_brand_stock
-	"Number of #{$brand_title} products: #{$brand_stock}"
+	 "Number of #{$brand_title} products: #{$brand_stock}"
 end
 
 def calculate_average__brand_price(retail_price, brand_stock)
 	$average__brand_price = (retail_price/brand_stock).round(2)
-	print_average__brand_price
-end
-
-def print_average__brand_price
-	"Average price of #{$brand_title} toys $#{$average__brand_price}"
+	"Average price of #{$brand_title} toys $#{(retail_price/brand_stock).round(2)}"
 end
 
 def calculate_brand_revenue
-	"Total #{$brand_title} revenue: $#{($sale_price).round(2)}"
+	 "Total #{$brand_title} revenue: $#{($sale_price).round(2)}"
 end
 
 start
